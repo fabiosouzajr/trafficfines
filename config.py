b488141020c4dd0961cbac10b91b6ea12308a89a
fabiosouzajr <fabiosouzajr@gmail.com>
2025-04-27 13:55:30 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+import os
+
+# Database configuration
+DATABASE_PATH = 'traffic_fines.db'
+
+# Google Calendar API configuration
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+CREDENTIALS_FILE = 'credentials.json'
+TOKEN_FILE = 'token.pickle'
+
+# PDF scanning configuration
+DEFAULT_PDF_FOLDER = os.path.join(os.path.expanduser('~'), 'Documents', 'TrafficFines')
